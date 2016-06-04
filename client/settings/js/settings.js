cb4fd3658846a9efe6bcea73bb77ef0347bbc5fe
Vishal Jain <vishaljain@college.harvard.edu>
2016-06-04 16:32:24 -0700

    check password

--- client/settings/js/settings.js
+++ client/settings/js/settings.js
@@ -15,6 +15,38 @@ Template.settings.events({
 	'click [data-action=read]': function() {
 		Meteor.call("readNotification", this._id);
     },
+
+    'submit #changePassForm': function(event){
+        event.preventDefault();
+        var oldPassword = event.target.oldPass.value;
+        var newPassword = event.target.newPass.value;
+        var newConfirm = event.target.newPassConfirm.value;
+        if (Session.get("isFB")){
+            alert("You logged in with FB!");
+            Session.set("showChangePassword", false);
+            // $("#changePassword").hide();
+        }
+        else if (newPassword == newConfirm){
+            Accounts.changePassword(oldPassword, newPassword, function(err){
+                if (err){
+                    alert(err.reason);
+                    Session.set("showChangePassword", false);
+                    // $("#changePassword").hide();
+                }
+                else{
+                    console.log("success");
+                    Session.set("showChangePassword", false);
+                    // $("#changePassword").hide();
+                }
+            });
+        }
+        else{
+            //TODO Send error to user
+            alert("Passwords no not match");
+            Session.set("showChangePassword", false);
+            // $("#changePassword").hide();
+        }
+    },
 });
 
 
